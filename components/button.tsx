34a811e072a6cf5385045c89487a7f3e90d7ffbd
Danimexivasco <dcano9493@gmail.com>
2024-03-05 09:41:32 +0100

    Create button component

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,23 @@
+import { combine } from "@/utils/combineClassNames"
+import { GENERAL_BUTTON_CLASSES } from "@/utils/constants"
 import { MouseEventHandler } from "react"
 
 export interface ButtonProps {
   text: string,
-  onClick: MouseEventHandler<HTMLButtonElement>
-  className: string
+  type?: "button" | "submit" | "reset"
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  isTab?: boolean
+  className?: string
+  disabled?: boolean
 }
 
-const Button = ({ text, onClick, className }: ButtonProps) =>
-  <button onClick={onClick} className={className}>{text}</button>
+const Button = ({ text, type, onClick, isTab=false, className, disabled }: ButtonProps) =>
+  <button
+    type={type}
+    disabled={disabled}
+    onClick={onClick}
+    className={combine(!isTab && GENERAL_BUTTON_CLASSES, className)}
+  >{text}</button>
 
 
 export default Button
