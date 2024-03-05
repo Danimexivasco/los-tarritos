20e7f44285d5d9b611e0536c1ec728f6701f76b7
Danimexivasco <dcano9493@gmail.com>
2024-03-05 09:36:03 +0100

    Create constants file

---
+++ utils/constants.ts
@@ -0,0 +1,28 @@
+// Routes
+export const ROUTES = {
+  HOME: "/",
+  NEW_ACTIVITY: "/activities/new",
+  EDIT_ACTIVITY: "/activities/:id"
+}
+
+export const AUTH_ROUTES = {
+  LOGIN: "/login",
+  REGISTER: "/register"
+}
+
+export const ACTIVITY_TYPES = {
+  SHORT: "Short", MEDIUM: "Medium", LARGE: "Large"
+}
+export const ACTIVITY_STATUS = {
+  TO_DO: "To do", IN_PROGRESS: "In progress", DONE: "Done"
+}
+
+export const ACTIVITY_TYPE_OPTIONS = Object.entries(ACTIVITY_TYPES).map(([ key, value ]) => ({ value: value, label: value }))
+export const ACTIVITY_STATUS_OPTIONS = Object.entries(ACTIVITY_STATUS).map(([ key, value ]) => ({ value: value, label: value }))
+
+
+// Tailwind Classes
+export const ACTIVE_TAB_CLASSES = "bg-cyan-500 text-white shadow-md shadow-white";
+export const INACTIVE_TAB_CLASSES = "bg-white hover:bg-cyan-100 text-black shadow-md";
+export const GENERAL_TAB_CLASSES = "p-4 rounded flex items-center justify-center text-lg font-bold";
+export const GENERAL_BUTTON_CLASSES = "w-full hover:bg-cyan-500/80 bg-cyan-500 text-white shadow-md shadow-white p-4 rounded flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed";
