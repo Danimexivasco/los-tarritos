d2da06150ef353cd3fd0bd90967c954476d95db3
Danimexivasco <dcano9493@gmail.com>
2024-09-16 13:13:30 +0200

    Change routes logic

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,15 +1,3 @@
-// Routes
-export const ROUTES = {
-  HOME: "/",
-  NEW_ACTIVITY: "/activities/new",
-  EDIT_ACTIVITY: "/activities/:id"
-}
-
-export const AUTH_ROUTES = {
-  LOGIN: "/login",
-  REGISTER: "/register"
-}
-
 export const ACTIVITY_TYPES = {
   SHORT: "Short", MEDIUM: "Medium", LARGE: "Large"
 }
@@ -26,3 +14,4 @@ export const ACTIVE_TAB_CLASSES = "bg-cyan-500 text-white shadow-md shadow-white
 export const INACTIVE_TAB_CLASSES = "bg-white hover:bg-cyan-100 text-black shadow-md";
 export const GENERAL_TAB_CLASSES = "p-4 rounded flex items-center justify-center text-lg font-bold";
 export const GENERAL_BUTTON_CLASSES = "w-full hover:bg-cyan-500/80 bg-cyan-500 text-white shadow-md shadow-white p-4 rounded flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed";
+export const MENU_CLASSES = " fixed left-0 right-0 z-40 overflow-hidden transition-all duration-500 ease-in-out shadow-neutral-800 shadow-md"
