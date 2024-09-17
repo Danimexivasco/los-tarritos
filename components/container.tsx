c5e42222b6d24c9c62ce3a25d71a208a80a87f44
Danimexivasco <dcano9493@gmail.com>
2024-09-17 11:59:49 +0200

    Adapt Container component

--- components/container.tsx
+++ components/container.tsx
@@ -1,9 +1,24 @@
 import { combine } from "@/utils/combineClassNames"
 import React from "react"
 
-const Container = ({ children, className, fullHeight = false }: {children: React.ReactNode, className?: string, fullHeight?: boolean}) => {
+export interface ContainerProps {
+  children: React.ReactNode
+  className?: string
+  fullHeight?: boolean
+  centered?: boolean
+}
+
+const Container = ({ children, className, fullHeight = false, centered = false } : ContainerProps) => {
   return (
-    <section className={combine("p-6 lg:p-12 max-w-7xl mx-auto", fullHeight && "min-h-[calc(100vh-7rem)] md:min-h-[calc(100vh-7.5rem)] h-full", className)}>{children}</section>
+    <section
+      className={combine("p-6 lg:p-12 max-w-7xl",
+        fullHeight && "min-h-[calc(100vh-7rem)] md:min-h-[calc(100vh-7.5rem)] h-full",
+        centered && "mx-auto",
+        className)
+      }
+    >
+      {children}
+    </section>
   )
 }
 
