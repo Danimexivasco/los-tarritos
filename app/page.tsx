8445b54c6b1a672f06ff6b95311daad0f80a28b3
Danimexivasco <dcano9493@gmail.com>
2024-09-17 11:59:56 +0200

    Finish home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import { ROUTES } from "@/utils/routes";
-import Link from "@/components/link";
 import { getPath } from "@/utils/getPath";
+import Link from "@/components/link";
 
 export default function Home() {
   return (
     <div className="min-h-[calc(100vh-10rem)] md:min-h-[calc(100vh-13.5rem)] h-full grid place-content-center">
       <h1 className="text-center text-4xl mb-8">Choose the desired APP</h1>
-      <div className="grid gap-4 place-content-center h-full">
+      <div className="grid md:flex gap-4 h-full">
         {ROUTES.filter(route => route.onMenu && route.name !== "Home").map(route =>
           <Link
             key={route.name}
             href={getPath(route.name)}
             asButton
-            className="text-2xl"
-          >{route.name}</Link>)}
+            className="text-2xl text-center"
+          >
+            {route.name}
+          </Link>)
+        }
       </div>
     </div>
   )
