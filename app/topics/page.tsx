f30358b86e8ac47d3255761242e1ddf08906f5f7
Danimexivasco <dcano9493@gmail.com>
2024-09-19 13:54:58 +0200

    Topics small adjusments

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -6,11 +6,11 @@ import React from "react"
 const Topics = ({}) => {
   return (
     <>
-      <h1 className="text-4xl my-8">Topics</h1>
+      <h1 className="text-4xl mb-8 font-bold">Topics</h1>
       <Link
         href={`${getPath("New Topic")}`}
-        className={"bg-emerald-500 hover:bg-emerald-700 w-full flex items-center justify-center rounded p-4 text-white my-8"}
-      >New Topic ➕</Link>
+        className={"font-bold bg-emerald-500 hover:bg-emerald-700 w-full flex items-center justify-center rounded p-4 text-white my-8"}
+      >NEW TOPIC ➕</Link>
       <TopicList />
     </>
   )
