73fc9030fc66a61bd6cb5f7c13a0e489d4fa8272
Danimexivasco <48056188+Danimexivasco@users.noreply.github.com>
2024-09-24 12:45:49 +0200

    Add svg icons (#12)
    * Add @svgr/webpack to enable svgs as react components
    
    * Change favicon
    
    * Add icons
    
    * Apply logo changes

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { Squash as Hamburger } from "hamburger-react"
 import Menu from "./menu";
+import AmphoraIcon from "@/public/icons/amphora.svg"
 
 const Header = () => {
   const [ isOpen, setOpen ] = useState(false)
@@ -9,7 +10,7 @@ const Header = () => {
   return(
     <div className="relative">
       <header className="h-14 md:h-16 bg-cyan-500 text-white flex items-center p-4 justify-between z-50 relative">
-        <h1 className="text-xs md:text-lg">Nuestros Tarritos 🏺</h1>
+        <h1 className="text-xs md:text-lg">Nuestros Tarritos <AmphoraIcon className="inline w-6 h-6"/></h1>
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </header>
       <Menu isOpen={isOpen} setOpen={setOpen}/>
