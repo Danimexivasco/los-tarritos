af6f1e67c8392231b976cff2f1c9cc7686451153
Danimexivasco <dcano9493@gmail.com>
2024-12-07 10:32:44 +0100

    Change good and bad points input to textarea

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,5 +1,5 @@
 import { combine } from "@/utils/combineClassNames"
-import React from "react"
+import React, { forwardRef } from "react"
 
 export interface TextAreaProps {
   required?: boolean,
@@ -11,31 +11,35 @@ export interface TextAreaProps {
   className?: string
 }
 
-const TextArea = ({ required=false, placeholder, value, label, onChange, name, className }: TextAreaProps) => {
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({ required=false, placeholder, value, label, onChange, name, className }, ref) => {
   return label ? (
     <div className="grid gap-2">
       <label><>{label}</></label>
       <textarea
+        ref={ref}
         required={required}
         name={name}
         placeholder={placeholder}
         value={value}
         rows={3}
         onChange={onChange}
-        className={combine("border-2 border-cyan-500/30 p-4 rounded focus:outline-none focus:border-cyan-500", className)}
+        className={combine("border-2 min-h-32 max-h-60 border-cyan-500/30 p-4 rounded focus:outline-none focus:border-cyan-500", className)}
       />
     </div>
   ) : (
     <textarea
+      ref={ref}
       required={required}
       name={name}
       placeholder={placeholder}
       value={value}
       rows={3}
       onChange={onChange}
-      className={combine("border-2 border-cyan-500/30 p-4 rounded focus:outline-none focus:border-cyan-500", className)}
+      className={combine("border-2 min-h-32 max-h-60 border-cyan-500/30 p-4 rounded focus:outline-none focus:border-cyan-500", className)}
     />
   )
-}
+})
+
+TextArea.displayName = "Textarea";
 
 export default TextArea
